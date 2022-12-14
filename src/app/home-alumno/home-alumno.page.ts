d2797f7847e9b3bcd92a527845188c58beb5ded4
Missael Padilla <lmpadillar@gmail.com>
2022-12-14 08:13:50 -0800

    Capacitor barcode

--- src/app/home-alumno/home-alumno.page.ts
+++ src/app/home-alumno/home-alumno.page.ts
@@ -4,7 +4,8 @@ import { Storage } from '@ionic/storage';
 import { User } from '../interfaces/user.interface';
 import { Attendance } from '../interfaces/asitencia.interface';
 import { AppService } from '../services/app.service';
-import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
+// import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-home-alumno',
@@ -12,65 +13,74 @@ import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
   styleUrls: ['./home-alumno.page.scss'],
 })
 export class HomeAlumnoPage implements OnInit, OnDestroy {
-
   user: User = {
     userName: '',
     password: '',
     email: '',
-    userType: ''
-  }
+    userType: '',
+  };
   userName = '';
   loading: boolean = true;
-  constructor(private storage: Storage, private router: Router, private service:AppService, private barcodeScanner: BarcodeScanner) {
+  constructor(
+    private storage: Storage,
+    private router: Router,
+    private service: AppService
+  ) {
     console.log('entra');
-    this.getLoggedUser().then( (user: User) => {
+    this.getLoggedUser().then((user: User) => {
       setTimeout(() => {
-        this.userName = user.userName
+        this.userName = user.userName;
         this.loading = false;
-        console.log(this.loading)
+        console.log(this.loading);
       }, 1500);
     });
   }
 
   async ngOnInit() {
-    await this.getLoggedUser().then( (user: User) => {
+    await this.getLoggedUser().then((user: User) => {
       setTimeout(() => {
-        this.userName = user.userName
-        console.log(this.loading)
+        this.userName = user.userName;
+        console.log(this.loading);
       }, 1500);
     });
-    console.log(this.loading)
+    console.log(this.loading);
   }
   ngOnDestroy(): void {
-    this.user= {
+    this.user = {
       userName: '',
       password: '',
       email: '',
-      userType: ''
-    }
+      userType: '',
+    };
     this.userName = '';
     this.loading = true;
-    console.log('destroy')
+    console.log('destroy');
   }
 
-  async registrarAsistencia(){
+  async registrarAsistencia() {
     const asistencia: Attendance = {
       name: this.userName,
       date: new Date(Date.now()),
       asignature: 'Desarrollo de Aplicaciones Moviles',
+    };
+
+    await BarcodeScanner.checkPermission({ force: true });
+    BarcodeScanner.hideBackground();
+
+    const result = await BarcodeScanner.startScan(); // start scanning and wait for a result
+
+    // if the result has content
+    if (result.hasContent) {
+      const data = result.content?.split(',');
+      if (data) {
+        asistencia.date = new Date(data[0]);
+        asistencia.asignature = data[1]; // log the raw scanned content
+      }
     }
-    this.barcodeScanner.scan().then(barcodeData => {
-      console.log('Barcode data', barcodeData);
-      const data = barcodeData.text.split(',');
-      asistencia.date = new Date(data[0]);
-      asistencia.asignature = data[1];
-    }).catch(err => {
-      console.log('Error', err);
-    });
     this.service.registrarAsistencia(asistencia);
   }
 
-  logOut(){
+  logOut() {
     this.service.logOut();
   }
 
